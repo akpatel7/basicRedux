e6b907b7c4db31f56df987fc156bb93ae21a40a5
Patel, Amitkumar <Amitkumar.Patel@tdassurance.com>
2019-09-09 15:18:40 -0400

    initial commit

---
+++ src/containers/Counter/Counter.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+    render () {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter(5)}  />
+                <CounterControl label="Subtract 8" clicked={this.props.onSubractCounter(8)}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(strResult => (
+                        <li onClick={this.props.onDeleteResult} key={strResult.id}>{strResult.value}</li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        ctr: state.counter,
+        storedResults: state.results
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: (val) => () => dispatch({type: 'ADD', val}),
+        onSubractCounter: (val) => () => dispatch({type: 'SUBTRACT', val}),
+        onStoreResult:  () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult:  () => dispatch({type: 'DELETE_RESULT'}),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
